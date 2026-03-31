2638afc12b0e187d6c9f1e40b601c4d61cca950c
Vladbraun <vlad@devlion.co>
2026-03-31 12:27:52 +0300

    (fix) import

--- backend/routes/rents.js
+++ backend/routes/rents.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const rentsController = require('../controllers/rentsController');
+const rentsController = require('../controllers/rentscontroller');
 const auth = require('../middleware/auth');
 
 router.get('/getRents', auth, rentsController.getRents);
